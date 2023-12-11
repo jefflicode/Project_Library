4d250bb5f68cc873c4a81ca0f81230eeb975ee94
jefflicode <jeffliswe@gmail.com>
2023-12-11 12:09:11 -0800

    class vs constructor function

--- scripts.js
+++ scripts.js
@@ -1,11 +1,20 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-  // the constructor
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+// function Book(title, author, pages, read) {
+//   // the constructor
+//   this.title = title;
+//   this.author = author;
+//   this.pages = pages;
+//   this.read = read;
+// }
+
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 
 function addBookToLibrary(book) {
